7ed85f9a2ac0677a2087dcd8e1c590290a5c5f24
lhiqwj173 <lhssqtz@gmail.com>
2021-05-18 08:25:25 +0800

    release

--- run.py
+++ run.py
@@ -7,21 +7,21 @@ import subprocess
 import time, datetime
 import logging
 import re
-
+#TODO 订阅请求失败
 class TestLog(object):
     def __init__(self,log_path,log_name, logger=None):
-        logger = logging.getLogger(logger)
-        logger.setLevel(logging.DEBUG)
+        self.logger = logging.getLogger(logger)
+        self.logger.setLevel(logging.DEBUG)
 
         log_time = time.strftime("%Y_%m_%d_")
         log_path = log_path
         log_name = os.path.join(log_path, log_time + log_name)
 
         fh = logging.FileHandler(log_name, 'a', encoding='utf-8')  # 这个是python3的
-        fh.setLevel(logging.INFO)
+        fh.setLevel(logging.DEBUG)
 
         ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
+        ch.setLevel(logging.DEBUG)
 
         formatter = logging.Formatter(
             # '[%(asctime)s] %(filename)s->%(funcName)s line:%(lineno)d [%(levelname)s]%(message)s')
@@ -29,13 +29,13 @@ class TestLog(object):
 
         fh.setFormatter(formatter)
         ch.setFormatter(formatter)
-        logger.addHandler(fh)
-        logger.addHandler(ch)
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
         fh.close()
         ch.close()
 
     def getlog(self):
-        return logger
+        return self.logger
 
 if not os.path.exists(r'/app/config/log'):
     os.mkdir(r'/app/config/log')
@@ -57,38 +57,42 @@ class auto_bbdown():
         # log = TestLog(self.abs_path,'auto_bbdown.log').getlog()
 
         # 删除5天前的日志
-        log.info('清理日志')
+        log.info('[COMMON]清理日志')
         five_day = datetime.datetime.now() - datetime.timedelta(days=5)
         # for i in os.listdir(self.abs_path):
         for i in os.listdir(r'/app/config/log'):
             if '.log' in i:
                 log_txt = re.search(r'\d{4}_\d{2}_\d{2}', i).group(0)
                 log_date = datetime.datetime.strptime(log_txt, '%Y_%m_%d')
-                log.debug('log_date:{}'.format(log_date))
+                log.debug('[COMMON]log_date:{}'.format(log_date))
                 if log_date < five_day:
                     # log_path = os.path.join(self.abs_path, i)
                     log_path = os.path.join(r'/app/config/log', i)
-                    log.debug('del_log:{}'.format(log_path))
+                    log.debug('[COMMON]del_log:{}'.format(log_path))
                     os.remove(log_path)
 
-        log.info('读取设置')
+        log.info('[COMMON]读取设置')
         self.conf = self.read_config()
-        log.debug('config_info:{}'.format(self.conf))
+        log.debug('[COMMON]config_info:{}'.format(self.conf))
         sleep_time = int(self.conf.get("common", "sleep_time"))
-        log.debug('休眠时间：{}'.format(sleep_time))
+        log.debug('[COMMON]休眠时间：{}'.format(sleep_time))
         while True:
             if self.check_time():
-                log.info('开始下载')
-                self.rss_data = self.get_rss_data()
-                if not self.rss_data:
-                    time.sleep(sleep_time)
+                log.info('[COMMON]开始运行')
+                try:
+                    self.rss_data = self.get_rss_data()
+                except:
+                    time.sleep(30)
                     continue
                 self.local_data = self.get_local_data()
+                self.local_title = []
+                for i in self.local_data:
+                    self.local_title.append(i.replace('.mp4',''))
                 self.del_items()
                 self.download()
                 time.sleep(sleep_time)
             else:
-                log.info('等待')
+                log.info('[COMMON]等待')
                 time.sleep(sleep_time)
     def get_py_path(self):
         return os.path.split(os.path.abspath(__file__))[0]
@@ -96,6 +100,7 @@ class auto_bbdown():
     def read_config(self,path: str = ''):
         if not path:
             path = os.path.join(r'/app/config', 'config.ini')
+        log.info("[COMMON]config_path:{}".format(path))
         if os.path.exists(path):
             conf = configparser.ConfigParser()
             try:
@@ -104,7 +109,18 @@ class auto_bbdown():
                 conf.read(path, encoding="utf-8")
             return conf
         else:
-            log.info("Config file not found!")
+            log.info("[COMMON]Config file not found!")
+            log.info("[COMMON]初始化空白 config.ini，请修改后再运行：docker start bbdown")
+            with open(path,'w')as f:
+                f.write(r'[common]' + '\n')
+                f.write(r'#rss链接，具体查看 https://docs.rsshub.app/social-media.html#bilibili' + '\n')
+                f.write(r'rss=' + '\n')
+                f.write(r"#运行时间段" + '\n')
+                f.write(r"#run_time='03:30-06:00','12:00-13:00'" + '\n')
+                f.write(r'run_time=all' + '\n')
+                f.write(r'#运行间隔 秒' + '\n')
+                f.write(r'sleep_time=300' + '\n')
+
             sys.exit(2)
 
     def check_time(self):
@@ -115,9 +131,9 @@ class auto_bbdown():
             run_time_list = ['00:00-23:59']
         else:
             run_time_list = [run_time]
-        log.debug('运行时间设定：{}'.format(str(run_time_list)))
+        log.debug('[CHECKTIME]运行时间设定：{}'.format(str(run_time_list)))
         n_time = datetime.datetime.now()
-        log.debug('now_time：{}'.format(n_time))
+        log.debug('[CHECKTIME]now_time：{}'.format(n_time))
         for i in run_time_list:
             l = i.split('-')
             log.debug('str(l)')
@@ -134,7 +150,7 @@ class auto_bbdown():
         url = self.conf.get("common", "rss")
         result = requests.get(str(url))
         if 200 != result.status_code:
-            log.info('请求异常')
+            log.info('[RSS_DATA]请求异常')
             return None
         r = result.content
         DOMTree = parseString(r)
@@ -152,9 +168,9 @@ class auto_bbdown():
 
             dict[title] = link
 
-            log.info(title)
-            log.info(link)
-            log.info(author)
+            log.info('[RSS_DATA]title:{}'.format(title))
+            log.info('[RSS_DATA]link:{}'.format(link))
+            log.info('[RSS_DATA]author:{}'.format(author))
 
         return dict
 
@@ -163,6 +179,8 @@ class auto_bbdown():
         path = r'/app/downloads'
         files = os.listdir(path)
         if len(files) > 0:
+            for i in files:
+                log.info('[LOCAL_DATA]{}'.format(i))
             return files
         else:
             return []
@@ -170,11 +188,17 @@ class auto_bbdown():
     def del_items(self):
         #path = self.conf.get("common", "download_path")
         path = r'/app/downloads'
+
         for i in self.local_data:
-            if i not in self.rss_data:
+            title = i.replace('.mp4', '')
+            if title not in self.rss_data.keys():
+                log.debug('[DEL]{}'.format(title))
                 file_path = os.path.join(path,i)
-                log.info('删除文件:{}'.format(file_path))
-                shutil.rmtree(file_path)
+                log.info('[DEL]删除文件:{}'.format(file_path))
+                if os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+                elif os.path.isfile(file_path):
+                    os.remove(file_path)
 
     def download(self):
         # BBDown_path = self.conf.get("common", "BBDown_path")
@@ -182,17 +206,18 @@ class auto_bbdown():
         # download_path = self.conf.get("common", "download_path")
         download_path = r'/app/downloads'
         for i in self.rss_data:
-            if i not in self.local_data:
+            if i not in self.local_title:
                 link = self.rss_data[i]
-                log.info('下载:{}'.format(i))
+                log.info('[DOWNLOAD]下载:{}'.format(i))
                 command = str(BBDown_path) + ' -p ALL "' + str(link) + '"'
-                print(command)
+                log.info('[DOWNLOAD]command:{}'.format(command))
                 self.run_cmd(command)
                 file_path = os.path.join(os.getcwd(),i)
                 path_d = os.path.join(download_path,i)
                 if not os.path.exists(file_path):
                     file_path = os.path.join(os.getcwd(), i + '.mp4')
                     path_d = os.path.join(download_path, i + '.mp4')
+                log.info('[DOWNLOAD]移动:{} --> {}'.format(file_path,path_d))
                 shutil.move(file_path,path_d)
     @staticmethod
     def run_cmd(command):
